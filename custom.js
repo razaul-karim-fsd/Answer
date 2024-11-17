c01229dba9dd64f6de2a96e7590a0b0c02411d69
razaul-karim-fsd <razaul.karim.fsd@gmail.com>
2024-11-17 21:10:20 +0600

    1st commite

---
+++ custom.js
@@ -0,0 +1,57 @@
+
+// QUESTION 1.
+
+let name = "John";
+let age = "25";
+
+console.log("My name is " + name + " and I am " + age + " Years old.");
+
+
+
+// QUESTION 2.
+
+let num = 5;
+if (num % 2 == 0) {
+    console.log("This number is even.");
+}
+else
+{
+    console.log("This number is odd.");
+}
+
+
+// QUESTION 3.
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+
+// QUESTION 4.
+
+function addNumbers(a, b) {
+    return sum = a + b
+}
+
+addNumbers(3, 7);
+console.log("This is tow numbers sum " + sum);
+
+
+// QUESTION 5.
+
+let fruits = ["apple", "banana", "cherry"];
+
+fruits.push("orange");
+
+console.log("The first fruit is:", fruits[0]);
+console.log("Total number of fruits:", fruits.length);
+
+
+// QUESTION 6.
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(function(number) {
+  console.log(number * 2);
+});
+
+
